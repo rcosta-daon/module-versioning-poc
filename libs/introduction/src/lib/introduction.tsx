c6de5d595858bb5ba5c58c86c4f2c6fc12b07192
Rodrigo Fernandes da Costa <rcosta@daon.com>
2025-07-07 15:56:16 +0100

    feat: add utils and introduction libraries

---
+++ libs/introduction/src/lib/introduction.tsx
@@ -0,0 +1,11 @@
+import styles from './introduction.module.css';
+
+export function ModuleVersioningPocIntroduction() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to ModuleVersioningPocIntroduction!</h1>
+    </div>
+  );
+}
+
+export default ModuleVersioningPocIntroduction;
