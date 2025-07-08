f99175d3e90813b9a21ea66668148e9927022ecb
Rodrigo Fernandes da Costa <rcosta@daon.com>
2025-07-08 15:22:56 +0100

    feat: add about page

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,28 +1,39 @@
-import { ModuleVersioningPocIntroduction } from '@module-versioning-poc/introduction';
 import { utils } from '@module-versioning-poc/utils';
 import { Route, Routes, Link } from 'react-router-dom';
+import { lazy } from 'react';
+
+const About = lazy(() => import('@module-versioning-poc/about'));
+const Introduction = lazy(() => import('@module-versioning-poc/introduction'));
 
 export function App() {
   return (
     <>
       <h1>Welcome to Client!</h1>
+
       <p>{utils()}</p>
-      <div>
-        <div role="navigation">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/page-2">Page 2</Link>
-            </li>
-          </ul>
-        </div>
-        <Routes>
-          <Route path="/" element={<p>Home</p>} />
-          <Route path="/page-2" element={<ModuleVersioningPocIntroduction />} />
-        </Routes>
-      </div>
+
+      <nav role="navigation">
+        <ul>
+          <li>
+            <Link to="/module-versioning-poc/">Home</Link>
+          </li>
+          <li>
+            <Link to="/module-versioning-poc/about">About</Link>
+          </li>
+          <li>
+            <Link to="/module-versioning-poc/introduction">Introduction</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/module-versioning-poc/" element={<p>Home</p>} />
+        <Route path="/module-versioning-poc/about" element={<About />} />
+        <Route
+          path="/module-versioning-poc/introduction"
+          element={<Introduction />}
+        />
+      </Routes>
     </>
   );
 }
